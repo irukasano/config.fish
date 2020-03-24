08408e8f736d96db3c2ceed3039761830fc902e4
sano <sano@iruka-system.co.jp>
2020-03-24 17:32:38 +0900

    add LESS_TERMCAP

--- config.fish
+++ config.fish
@@ -62,3 +62,11 @@ set -x LESS "-R"
 set -x LESSOPEN "||/usr/bin/lesspipe.sh %s"
 set -x LESSCLOSE "~/dotfiles/lessclose.sh %s %s"
 
+set -x LESS_TERMCAP_mb (printf "\e[1;32m")
+set -x LESS_TERMCAP_md (printf "\e[1;32m")
+set -x LESS_TERMCAP_me (printf "\e[0m")
+set -x LESS_TERMCAP_se (printf "\e[0m")
+set -x LESS_TERMCAP_so (printf "\e[01;33m")
+set -x LESS_TERMCAP_ue (printf "\e[0m")
+set -x LESS_TERMCAP_us (printf "\e[1;4;31m")
+
